c83a3a39aac6370fa403de0cff63c5cb8ef675cd
Daniel Brook-Roberge <daniel@vclabs.ca>
2017-09-14 14:18:56 -0700

    Add the infix conversion service

---
+++ infix/index.js
@@ -0,0 +1,68 @@
+const {Stack, List} = require('immutable')
+const isNumber = require('lodash.isnumber')
+const {json, createError} = require('micro')
+
+const precedence = {
+  '^': 3,
+  '*': 2,
+  '/': 2,
+  '-': 1,
+  '+': 1,
+  '(': 0
+}
+
+const pop = (stack) => [stack.peek(), stack.pop()]
+
+const transferBracketOperators = (stack, output) => {
+  if (stack.size <= 0) {
+    throw createError(400, 'Stack underflow')
+  }
+
+  const [operator, st1] = pop(stack)
+  if (operator === '(') {
+    return [st1, output]
+  }
+  return transferBracketOperators(st1, output.push(operator))
+}
+
+const resolvePrecedence = (operator, stack, output) => {
+  const opPrecedence = precedence[operator]
+  const stackPrecedence = precedence[stack.peek()] || 0
+  if (opPrecedence < stackPrecedence) {
+    const [stackOp, st1] = pop(stack)
+    return resolvePrecedence(operator, st1, output.push(stackOp))
+  }
+  if (opPrecedence === stackPrecedence) {
+    const [stackOp, st1] = pop(stack)
+    return [st1.push(operator), output.push(stackOp)]
+  }
+  if (opPrecedence > stackPrecedence) {
+    return [stack.push(operator), output]
+  }
+}
+
+const exhaustStack = (stack, output) => {
+  if (stack.size <= 0) {
+    return output
+  }
+  return exhaustStack(stack.pop(), output.push(stack.peek()))
+}
+
+const convertInfix = (tokens) => {
+  const [finalStack, finalOutput] = tokens.reduce(([stack, output], token) => {
+    if (isNumber(token)) {
+      return [stack, output.push(token)]
+    }
+    if (token === '(') {
+      return [stack.push(token), output]
+    }
+    if (token === ')') {
+      return transferBracketOperators(stack, output)
+    }
+    return resolvePrecedence(token, stack, output)
+  }, [Stack(), List()])
+
+  return exhaustStack(finalStack, finalOutput)
+}
+
+module.exports = async (req) => convertInfix(await json(req))
