a4dfb2ea56444a7bb56a8c97495d899adfe32237
Daniel Brook-Roberge <daniel@vclabs.ca>
2017-09-14 13:53:10 -0700

    Add the calculation service.

---
+++ rpn/index.js
@@ -0,0 +1,40 @@
+const isNumber = require('lodash.isnumber')
+const {createError, json} = require('micro')
+const {Stack} = require('immutable')
+
+const binaryOperators = {
+  '+': (op1, op2) => op2 + op1,
+  '-': (op1, op2) => op2 - op1,
+  '*': (op1, op2) => op2 * op1,
+  '/': (op1, op2) => op2 / op1,
+  '^': (op1, op2) => op2 ** op1
+}
+
+const pop = (stack) => [stack.peek(), stack.pop()]
+
+const evaluateRpn = (tokens) => {
+  const finalStack = tokens.reduce((stack, token) => {
+    if (isNumber(token)) {
+      return stack.push(token)
+    }
+
+    if (token in binaryOperators) {
+      if (stack.length < 2) {
+        throw createError(400, 'Stack underflow')
+      }
+      const [op1, st1] = pop(stack)
+      const [op2, st2] = pop(st1)
+      return st2.push(binaryOperators[token](op1, op2))
+    }
+
+    throw createError(400, `Unknown token ${token}`)
+  }, Stack())
+
+  if (finalStack.size > 1) {
+    throw createError(400, `Multiple values (${JSON.stringify(finalStack.toJS())}) remaining on stack`)
+  }
+
+  return finalStack.peek()
+}
+
+module.exports = async (req) => evaluateRpn(await json(req))
