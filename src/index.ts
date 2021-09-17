a40f88f2a03e87a585566ab0e220af023ba28b27
Danielle Brook-Roberge <danielle.brook-roberge@unbounce.com>
2021-09-17 10:35:29 -0700

    Make a basic local RPN calculator.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,45 @@
+const evaluateRpn = (tokens: (string | number)[]): number => {
+  const stack: number[] = [];
+  for (const token of tokens) {
+    if (typeof token === "number") {
+      stack.push(token);
+    } else {
+      const op2 = stack.pop();
+      const op1 = stack.pop();
+
+      if (op1 === undefined || op2 === undefined) {
+        throw "Stack underflow";
+      }
+
+      switch (token) {
+        case "+":
+          stack.push(op1 + op2);
+          break;
+        case "-":
+          stack.push(op1 - op2);
+          break;
+        default:
+          throw "Unknown operator";
+      }
+    }
+  }
+  return stack[0];
+};
+
+// We're reading our command line here so tokenize doesn't take arguments yet
+const tokenize = (): (string | number)[] => {
+  const expression = [...process.argv];
+  // Clear out the program name
+  expression.shift();
+  expression.shift();
+  return expression.map((item) => {
+    const parsed = parseInt(item);
+    return isNaN(parsed) ? item : parsed;
+  });
+};
+
 function main() {
-  console.log("1 + 1", 1 + 1);
+  console.log(evaluateRpn(tokenize()));
 }
 
 main();
